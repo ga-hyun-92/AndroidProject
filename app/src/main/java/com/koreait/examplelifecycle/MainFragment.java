9f42f67a5da295ef9a80eaf52b2aee647ee9dd20
DESKTOP-NUOPUIF\ITPS <hyunsybm@daum.net>
2021-05-24 18:26:32 +0900

    ExamplLifeCycle

---
+++ app/src/main/java/com/koreait/examplelifecycle/MainFragment.java
@@ -0,0 +1,14 @@
+package com.koreait.examplelifecycle;
+
+import androidx.fragment.app.Fragment;
+
+//부모 클래스인 Fragment에는 setContentView메서드가 없음
+//그래서 Activity처럼 레이아웃을 연결할 수 없음
+public class MainFragment extends Fragment {
+    //프래그먼트를 위한 XML파일을 인플레이션한 뒤
+    //onCreateView메서드를 통해서 인플레이션된 프래그먼트의 화면을 사용하도록 한다.
+    //onCreateView메서드는 콜백 메서드
+    //onCreateView메서드 안에 XML파일을 인플레이션하는 코드를 작성해야한다.
+    //inflate()-> XML파일을 인플레이션하는 코드
+
+}
